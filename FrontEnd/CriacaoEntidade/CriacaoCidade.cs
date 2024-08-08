33709d988c56b6bee90ed5f8a5e3babee21b2cd5
Fred Marques <fredyn.curso@gmail.com>
2024-08-08 07:49:13 -0300

    cmt

---
+++ FrontEnd/CriacaoEntidade/CriacaoCidade.cs
@@ -0,0 +1,27 @@
+﻿using CRUD.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEnd.CriacaoEntidade
+{
+    public class CriacaoCidade
+    {
+        public CriacaoCidade()
+        { 
+
+        }
+
+        private Cidade CriarCidade()
+        {
+            Cidade cidadeAux = new Cidade();
+            Console.WriteLine("Digite o nome da cidade:");
+            cidadeAux.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o número de habitantes da cidade:");
+            cidadeAux.NumHabitatntes = int.Parse(Console.ReadLine());
+            return cidadeAux;
+        }
+    }
+}
