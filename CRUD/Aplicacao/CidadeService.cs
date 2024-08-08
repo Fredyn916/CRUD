2d821679f16f4387e31eb2dc101dd2dd7ea4561c
Fred Marques <fredyn.curso@gmail.com>
2024-08-08 07:20:51 -0300

    TérminoClasses

---
+++ CRUD/Aplicacao/CidadeService.cs
@@ -0,0 +1,37 @@
+﻿using CRUD.Entidades;
+using CRUD.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Aplicacao
+{
+    public class CidadeService
+    {
+        public SimuladorBD bd { get; set; }
+        public CidadeRepository repository { get; set; }
+
+        public CidadeService(SimuladorBD bdPreenchido)
+        {
+            bd = bdPreenchido;
+            repository = new CidadeRepository(bd);
+        }
+
+        public void Adicionar(Cidade cidade)
+        {
+            repository.Adicionar(cidade);
+        }
+
+        public void Remover(Cidade cidade)
+        {
+            repository.Remover(cidade);
+        }
+
+        public List<Cidade> Listar()
+        {
+            return repository.Listar();
+        }
+    }
+}
---
+++ CRUD/Repositorios/CidadeRepository.cs
@@ -0,0 +1,36 @@
+﻿using CRUD.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Repositorios
+{
+    public class CidadeRepository
+    {
+        public SimuladorBD bd { get; set; }
+
+        public CidadeRepository(SimuladorBD bdPreenchido)
+        {
+            bd = bdPreenchido;
+        }
+
+        public void Adicionar(Cidade cidade)
+        {
+            bd.Cidades.Add(cidade);
+        }
+        public void Remover(Cidade cidade)
+        {
+            bd.Cidades.Remove(cidade);
+        }
+        public void Editar()
+        {
+
+        }
+        public List<Cidade> Listar()
+        {
+            return bd.Cidades.ToList();
+        }
+    }
+}
