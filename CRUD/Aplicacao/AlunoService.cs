b51e46d4a0a21d0ad11b2d30019593a32bc88994
Fred Marques <fredyn.curso@gmail.com>
2024-08-08 07:13:49 -0300

    Commit

---
+++ CRUD/Aplicacao/AlunoService.cs
@@ -0,0 +1,37 @@
+﻿using CRUD.Entidades;
+using CRUD.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Aplicacao
+{
+    public class AlunoService
+    {
+        public SimuladorBD bd { get; set; }
+        public AlunoRepository repository { get; set; }
+
+        public AlunoService(SimuladorBD bdPreenchido)
+        {
+            bd = bdPreenchido;
+            repository = new AlunoRepository(bd);
+        }
+
+        public void Adicionar(Aluno aluno)
+        {
+            repository.Adicionar(aluno);
+        }
+
+        public void Remover(Aluno aluno)
+        {
+            repository.Remover(aluno);
+        }
+
+        public List<Aluno> Listar()
+        {
+            return repository.Listar();
+        }
+    }
+}
--- CRUD/SimuladorBD.cs
+++ CRUD/SimuladorBD.cs
@@ -10,9 +10,11 @@ namespace CRUD
     public class SimuladorBD
     {
         public List<Time> Times { get; set; }
+        public List<Aluno> Alunos { get; set; }
         public SimuladorBD()
         {
             Times = new List<Time>();
+            Alunos = new List<Aluno>();
         }
     }
 }
